4f5a9ec4fcd00995578ae61ced861a6556ca6cee
sambell <bell.sam@Outlook.com>
2020-06-20 21:58:33 -0500

    adjusted middleware logic

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -2,7 +2,6 @@ import constants from "../assets/constants.js"
 import Cookie from "js-cookie"
 
 export default function (context) {
-    if (!context.store.getters["authentication/isAuthenticated"]) {
         const jwt = Cookie.get(constants.authCookieName);
         if (jwt) {
             context.$axios.defaults.headers.common['Authorization'] = 'Bearer ' + jwt;
@@ -10,13 +9,4 @@ export default function (context) {
         } else {
             context.redirect("/login")
         }
-    } else {
-        const jwt = Cookie.get(constants.authCookieName);
-        if (jwt) {
-            context.$axios.defaults.headers.common['Authorization'] = 'Bearer ' + jwt;
-            context.store.commit("authentication/saveJWT", jwt);
-        } else {
-            context.redirect("/login")
-        }
-    }
 }
